07e0f1000015c435b9c1f36c6627c76345f4150e
Ayush Dalia <39301958+adalia2799@users.noreply.github.com>
2021-06-17 11:51:30 +0530

    Update helloworld.c

--- ccode/helloworld.c
+++ ccode/helloworld.c
@@ -11,8 +11,13 @@ void dingDong(){
 	printf("SOMething ding dong.....")
 }
 
+void greeting(char name[]){
+          printf("hello, Where were you this is the remote one?... %s",name);
+}
 
 int main(){
+	char name[] = "deepika padukone";
 	helloworld();
+	greeting(name);
 }
 
