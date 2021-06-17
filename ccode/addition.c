2f7a2f21cb163ec756756b2af73d4cc8e779cc3e
adalia2799 <ayushdalia27@gmail.com>
2021-06-17 10:21:21 +0530

    On branch main
     Changes to be committed:
    	modified:   ccode/addition.c
    	modified:   javacode/hello.java

--- ccode/addition.c
+++ ccode/addition.c
@@ -10,6 +10,10 @@ int add3(int a,int b,int c){
 	return a+b+c;
 }
 
+int add4(int a,int b,int c,int d){
+	return a+b+c+d;
+}
+
 int main(){
 	int res = add(5,8);
 	printf(res);
--- javacode/hello.java
+++ javacode/hello.java
@@ -5,6 +5,7 @@ package experiment;
 public class hello{
 	public static void main(String[] args){
 		System.out.println("HEllo world");
+		System.out.println("You are sexy af");
 	}
 }
 
