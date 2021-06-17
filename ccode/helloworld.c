b666baa22da6ba0819f43e8c2480bfd5b7493386
adalia2799 <ayushdalia27@gmail.com>
2021-06-17 11:53:57 +0530

    Changes committed via local side: 	modified:   ccode/helloworld.c 	modified:   javacode/hello.java

--- ccode/helloworld.c
+++ ccode/helloworld.c
@@ -11,8 +11,13 @@ void dingDong(){
 	printf("SOMething ding dong.....")
 }
 
+void greeting(char name[]){
+	printf("hello, Where were you?... %s",name);
+}
 
 int main(){
+	char name[] = "alia bhatt";
 	helloworld();
+	greeting(name);
 }
 
--- javacode/hello.java
+++ javacode/hello.java
@@ -7,6 +7,9 @@ public class hello{
 		System.out.println("HEllo world");
 		System.out.println("You are sexy af");
 	}
+	public void greeting(String name){
+		System.out.println("good afternoon " + name);
+	}
 }
 
 
