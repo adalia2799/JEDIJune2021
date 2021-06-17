0cdd68876b94d4b0ff4e7920eb4f0f9dddaeb5cb
Ayush Dalia <39301958+adalia2799@users.noreply.github.com>
2021-06-17 11:52:36 +0530

    updated hello.java via remote

--- javacode/hello.java
+++ javacode/hello.java
@@ -2,10 +2,19 @@
 
 package experiment;
 
+
+
 public class hello{
 	public static void main(String[] args){
 		System.out.println("HEllo world");
 		System.out.println("You are sexy af");
+	}
+	
+	public void namaste(){
+	
+	
+		System.out.println("namaste world!!");
+	
 	}
 }
 
